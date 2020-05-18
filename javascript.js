acb10ef9cccee09b2e59ecb9b38493b1337d1bf8
RawBaconOnAStick <rohan.dharwadker@gmail.com>
2020-05-18 13:27:01 -0500

    First Commit

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
